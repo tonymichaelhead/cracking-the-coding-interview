a1ee9df988192ae9ad1f5aaaafedb371abadf235
tonymichaelhead <tonymichaelhead@gmail.com>
2017-10-07 16:21:37 +0900

    Solve urlify

---
+++ urlify.js
@@ -0,0 +1,16 @@
+/* 
+URLify : Write a method to replace all spaces in a string with '%20  You may assume that the string has suf cient space at the end to hold the additional characters,and that you are given the "true" length of the string. (Note: If implementing in Java,please use a character array so that you can perform this operation in place.)
+*/
+
+const urlify = (string) => {
+    let newString = '';
+   
+    string.trim().split('').forEach(char => {
+      if(char === ' ') {
+        newString += '&20';
+      } else {
+        newString += char;
+      }
+    });
+    return newString;
+}
